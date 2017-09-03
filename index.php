05fc06c12b2bcb1b77222aea780b2d98da1c2ccb
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-09-03 14:49:08 -0400

    add schreenshot.php; add wp login form and basic styling

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-
-wp_login_form();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+    <div id="page">
+        <?php wp_login_form(); ?>
+    </div>
+</body>
+</html>
