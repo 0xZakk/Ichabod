246928aa0a2f948a5c2191fec3017e76c7f523b9
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-09-03 15:19:17 -0400

    switch to is_user_logged_in(), move container and title

--- index.php
+++ index.php
@@ -9,24 +9,24 @@
 </head>
 <body>
     <div id="page">
-        <?php if (wp_get_current_user()): ?>
-            <div class="container">
-                <h1><?php bloginfo('name'); ?></h1>
-                <ul class="menu">
-                    <li>
-                        <a href="<?php echo admin_url(); ?>">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo admin_url('edit.php'); ?>">Posts</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo admin_url('edit.php?post_type=page'); ?>">Pages</a>
-                    </li>
-                </ul>
-            </div>
-        <?php else: ?>
-            <?php wp_login_form(); ?>
-        <?php endif;?>
+        <div class="container">
+            <h1><?php bloginfo('name'); ?></h1>
+            <?php if (is_user_logged_in()): ?>
+                    <ul class="menu">
+                        <li>
+                            <a href="<?php echo admin_url(); ?>">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo admin_url('edit.php'); ?>">Posts</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo admin_url('edit.php?post_type=page'); ?>">Pages</a>
+                        </li>
+                    </ul>
+            <?php else: ?>
+                <?php wp_login_form(); ?>
+            <?php endif;?>
+        </div>
     </div>
 </body>
 </html>
